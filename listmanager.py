2ca114694e70bbc56deddbea89552730934ad287
ClientSiderz <>
2025-01-02 01:41:17 +0200

    Made a session saving and loading system, as well a clear function for list management

--- listmanager.py
+++ listmanager.py
@@ -1,88 +1,110 @@
 import os
+import json
 import platform
 
 list = []
 
 #List Managment Functions
-        
 def addNumber():
     print("Enter a number to add to the list: ")
-    
+
     number = int(input("> "))
     list.append(number)
-    
+
     clearTerminal()
-    
+
 def removeNumber():
     try:
         clearTerminal()
-                
+
         print("Select which number to remove by entering their count: ")
-        
+
         for n in list:
             print(f"{list.index(n) + 1} - {n}")
-            
+
         number = int(input("> "))
         list.remove(list[(number - 1)])
-        
+
     except IndexError:
         clearTerminal()
         print("Invalid input, Please enter a valid number.")
-    
+
 # Number Management Functions
 
 def manageListNumber():
     try:
         clearTerminal()
-        
+
         print("Which number would you like to manage?")
-        
+
         for n in list:
             print(f"{list.index(n) + 1} - {n}")
-            
+
         number = int(input("> "))
-        
+
         print("Which mathematical operation would you like to do?")
-        
+
         operation = int(input("1 - Addition\n2 - Subtract\n3 - Multiply\n4 - Divide\n> "))
-        
+
         match operation:
             case 1:
                 clearTerminal()
-                
+
                 print("Enter a number to add to the selected number: ")
-                
+
                 add = int(input("> "))
                 list[(number - 1)] += add
             case 2:
                 clearTerminal()
-                
+
                 print("Enter a number to subtract to the selected number: ")
-                
+
                 sub = int(input("> "))
                 list[(number - 1)] -= sub
             case 3:
                 clearTerminal()
-                
+
                 print("Enter a number to multiply to the selected number: ")
-                
+
                 mul = int(input("> "))
                 list[(number - 1)] *= mul
             case 4:
                 clearTerminal()
-                
+
                 print("Enter a number to divide to the selected number: ")
-                
+
                 div = int(input("> "))
                 list[(number - 1)] /= div
             case _:
                 clearTerminal()
                 print("Invalid input, Please enter a valid")
-                    
+
     except IndexError:
         clearTerminal()
         print("Invalid input, Please enter a valid number.")
+
+# List last session load
+
+def saveSession():
+    os.makedirs(".listmanager", exist_ok=True)
     
+    filepath = os.path.join(".listmanager", "session.json")
+
+    with open(filepath, 'w') as file:
+        json.dump(list, file)
+
+def loadSession():
+    filepath = os.path.join(".listmanager", "session.json")
+
+    try:
+        with open(filepath, 'r') as file:
+            numbers = json.load(file)
+        return numbers
+    except FileNotFoundError:
+        print("Error: Last session not found, a new session file will be made when exited.")
+        return []
+
+list = loadSession()
 
 # Utility Functions
 
@@ -104,7 +126,8 @@ while True:
 2 - Remove a number
 3 - Manage numbers in the list
 4 - Sort the list
-5 - Exit
+5 - Clear the list
+6 - Exit
     ''')
     try:
         choice = int(input("> "))
@@ -123,9 +146,13 @@ while True:
                     print("Operation failed, the list is empty")
             case 4:
                 clearTerminal()
-                
+
                 list.sort()
             case 5:
+                clearTerminal()
+                list = []
+            case 6:
+                saveSession()
                 print("Goodbye!")
                 break
             case _:
